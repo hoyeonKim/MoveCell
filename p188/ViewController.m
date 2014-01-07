8f20a9642d3d2c8de55ebe7a55a7b3609b692a1a
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-07 21:54:01 +0900

    MoveCell
    완료했습니

--- p188/ViewController.m
+++ p188/ViewController.m
@@ -7,16 +7,68 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#define CELL_ID @"CELL_ID"
+@interface ViewController ()<UITextFieldDelegate, UITableViewDataSource, UITabBarDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *userInput;
+@property (weak, nonatomic) IBOutlet UITableView *table;
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSMutableArray *data;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [data count];
+}
 
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+    cell.textLabel.text = [data objectAtIndex:indexPath.row];
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(editingStyle ==UITableViewCellEditingStyleDelete){
+        [data removeObjectAtIndex:indexPath.row];
+        NSArray *rows = [NSArray arrayWithObject:indexPath];
+        [self.table deleteRowsAtIndexPaths:rows withRowAnimation:UITableViewRowAnimationAutomatic];
+    }
+}
+
+-(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+- (IBAction)addItem:(id)sender {
+    NSString *inputstr = self.userInput.text;
+    if([inputstr length]>0){
+        [data addObject:inputstr];
+        
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:([data count]-1) inSection:0];
+        NSArray *row = [NSArray arrayWithObject:indexPath];
+        [self.table insertRowsAtIndexPaths:row withRowAnimation:UITableViewRowAnimationAutomatic];
+        
+        self.userInput.text = @"";
+    }
+}
+- (IBAction)toggleEditMode:(id)sender {
+    self.table.editing = ! self.table.editing;
+    ((UIBarButtonItem*)sender).title = self.table.editing? @"Done":@"Edit";
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    [self addItem:nil];
+    return YES;
+}
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+    NSObject *obj = [data objectAtIndex:sourceIndexPath.row];
+    [data removeObjectAtIndex:sourceIndexPath.row];
+    [data insertObject:obj atIndex:sourceIndexPath.row];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    data = [[NSMutableArray alloc]init];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
